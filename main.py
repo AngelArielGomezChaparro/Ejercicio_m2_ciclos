29f54ea6e0faad2a7f78c0de5059a84a538169cd
AngelArielGomezChaparro <182712613+AngelArielGomezChaparro@users.noreply.github.com>
2024-11-06 11:59:25 -0500

    solution excercise 4

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-#Escriba un programa que pida al usuario dos números enteros, y luego entregue la suma de todos 
-# los números que están entre ellos. Por ejemplo, si los números son 1 y 7, debe entregar como 
-# resultado 2 + 3 + 4 + 5 + 6 = 20.
 
-print ("escriba dos numeros para sumar todos los q estan dentro de ellos")
-numero1 = int(input("escriba su primer numero: "))
-numero2 = int(input("escriba el segundo numero: "))
+for a in range(1, 11):
 
-# esta variante if es para q aun el numero primero sea mayor funcione el codigo de todos modos
+    for i in range(1, 11):
 
-if numero1 > numero2:
+        m = i * a
 
-    numero1, numero2 = numero2, numero1
-#El comando sum suma una secuencia de numeros proporcionados por el comando range
-suma = sum(range(numero1 + 1, numero2))
-
-print(F"La suma de los numero entre {numero1} y {numero2} = {suma}")
+        print(f"{m:4}", end=" ")
+    
+    print()
