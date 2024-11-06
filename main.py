90ea8f4a4fbd9e6d96306f5afb57d155d91d26eb
AngelArielGomezChaparro <182712613+AngelArielGomezChaparro@users.noreply.github.com>
2024-11-06 11:00:32 -0500

    solution exercise 1

---
+++ main.py
@@ -0,0 +1,11 @@
+
+numero = int(input("ingrese un numero para mostrar la tabla de multiplicar: "))
+
+
+
+for i in range (1, 11):
+
+    resultado = numero * i
+
+    print(f"{numero} X {i} = {resultado}")
+    
